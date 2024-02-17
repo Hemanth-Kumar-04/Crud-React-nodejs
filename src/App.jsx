6ca53b4262b69844d827d8cc0480d281dbb45ee5
Hemanth-Kumar-04 <hemanthkumar.k.r04@gmail.com>
2024-02-18 00:36:22 +0530

    the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500"> App </h1>      
+    </div>
+  )
+}
+
+export default App
